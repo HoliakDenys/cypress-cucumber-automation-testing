0b56086472c3c6af1bb4ecf8dd675fa5d3ae788c
HoliakDenys <denysholiak@gmail.com>
2025-03-31 20:18:48 +0300

    new commit

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,25 +2,30 @@ import { defineConfig } from 'cypress';
 import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
 import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
-require('dotenv').config();
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.feature',
     baseUrl: process.env.BASE_URL,
-    reporter: 'cypress-allure-plugin',
+    reporter: '@shelex/cypress-allure-plugin',
     reporterOptions: {
-      resultsDir: 'cypress/allure-results',
+      resultsDir: 'allure-results',
     },
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
+      
       on(
         'file:preprocessor',
         createBundler({
           plugins: [createEsbuildPlugin(config)],
         })
       );
-      require('@shelex/cypress-allure-plugin').install(on, config);
+      
+      require('@shelex/cypress-allure-plugin/writer')(on, config);
+      
       return config;
     },
   },
